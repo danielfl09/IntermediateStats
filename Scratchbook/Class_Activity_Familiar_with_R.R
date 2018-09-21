0e8557a57747bc0af633aa202b81f7aefdbb8e3d
danielfl09 <you@example.com>
2018-09-20 19:30:07 -0600

    update

--- Scratchbook/Class_Activity_Familiar_with_R.R
+++ Scratchbook/Class_Activity_Familiar_with_R.R
@@ -5,3 +5,11 @@ View(airquality)
 
 x <- 
   na.omit(airquality)
+
+x %>% 
+  ggplot(aes(y = Wind, group = Month, color = "steelblue")) +
+  geom_boxplot() +
+  theme_bw()
+
+
+boxplot(airquality$Wind)
